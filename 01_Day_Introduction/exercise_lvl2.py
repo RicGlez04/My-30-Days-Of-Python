e863eab60bdf27bdaa231e646b54f0b872b1efe0
Ricardo Sebastián González Rivas <rickyg.rivas@gmail.com>
2026-09-04 10:59:54 -0600

    Create exercise_lvl2.py

---
+++ 01_Day_Introduction/exercise_lvl2.py
@@ -0,0 +1,28 @@
+# Day 1 - 30DaysOfPython Challenge
+# Exercise Level 2
+
+# Math Operations
+print(3 + 4)   # addition(+)
+print(3 - 4)   # subtraction(-)
+print(3 * 4)   # multiplication(*)
+print(3 % 4)   # modulus(%)
+print(3 / 4)   # division(/)
+print(3 ** 4)  # exponential(**)
+print(3 // 4)  # Floor division operator(//)
+
+# Strings
+print('Ricky')      # My name
+print("Gonzalez")   # My family name
+print('Mexico')     # My country
+print('''I am enjoying 30 days of python''')
+
+# Checking data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Azabaneh', 'Python', 'Finland']))
+print(type('Ricky'))
+print(type("Gonzalez"))
+print(type('Mexico'))
+print(type('''I am enjoying 30 days of python'''))
